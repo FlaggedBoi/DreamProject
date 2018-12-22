01a483ece13b72a875899459395bb42bc6b23dbe
Hellwest <dushekrad@gmail.com>
2018-12-22 15:47:12 +0400

    Добавлен класс инвентаря

---
+++ Inventory.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamProject
+{
+  class Inventory
+  {
+    string itemID;
+    string itemName { get; };
+    string itemDescription { get; };
+    string itemProperties;
+  }
+}

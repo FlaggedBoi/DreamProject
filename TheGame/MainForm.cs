ec71814b851bf3c562b110402ef7d3a035305dd0
Влад Никитин <dushekrad@gmail.com>
2019-01-10 11:37:50 +0400

    Добавлены переходы: меню, игра, настройки

---
+++ TheGame/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheGame
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = GamePage;
+        }
+
+        private void SettingsButton_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = SettingsPage;
+        }
+
+        private void BackFromSettings_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = MenuPage;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BackFromGame_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = MenuPage;
+        }
+    }
+}
